e1211df16e9888534b5b79c3e9a7fe9d0d8ef60b
DCrust369 <DCrust@proton.me>
2026-04-25 23:16:39 +0100

    Create zig.zig
    security for kernel

---
+++ kernel/zig.zig
@@ -0,0 +1,39 @@
+const sdt = @import("std");
+
+pub fn main() !void {
+    // malware ficticio
+    try std.fs.File.stdout().writeAll("Malware!\n");
+    try std.fs.File.stdout().writeAll("tipo: ransoware!\n");
+    // malware name
+    try std.fs.File.stdout().writeAll("Mal que habita a terra!\n");
+    // force brute
+    var i64 = 10.000;
+    var senhas = 10.000;
+    var 10.000 = tentativas;
+    {
+        var i64 = c_char;
+        var i64 = 10.000;
+        const kernel = panic!;
+    }
+    const senha = quebrada!
+    var i64 = per = second;
+    const encryptografy = i64 = 86;
+    const E2EE = https://github.com/amnesica/KryptEY = i64;
+    // ok this ransoware in zig ohhh imagine a staff enginer
+    // see to my code zig is a new C
+    // agora em lingua portuguesa teremos a exploração!
+    const nmap = bash = sudo;
+    const nmap = bash = nmap;
+    const nmap = bash = windows;
+    // agora no MacOS
+    const nmap = bash = sudo;
+    const nmap = bash = nmap;
+    const nmap = bash = macos;
+    // in linux linux is a kernel not operational system 
+    // very, very brute end hard for DOS atacks or force brute
+    const nmap = bash = sudo;
+    const nmap = bash = nmap;
+    const nmap = bash = linux;
+    struct c_shar = nmap;
+    const sistema = explorado;
+}
