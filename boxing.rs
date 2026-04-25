dd64e139c5e28df47480b18e953fd598eef39adb
DCrust369 <DCrust@proton.me>
2026-04-25 23:52:35 +0100

    Create boxing.rs

---
+++ boxing.rs
@@ -0,0 +1,50 @@
+let mut verify_area = (void_*_addr,int_count);
+let mut void_panic = (const_char_*_str);
+let asm = (const char_*_fmt,...);
+let asm = (const_char * fmt,...);
+let tty_write = (unsigned_ch,char_*_buf,int_count);
+~
+~use::boxing;
+{
+  let brawser = (process(1)); // for boxes
+}
+{
+  let code = (process(2));
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+Relix-0.01v/Relix-0.01-mega/include/linux/kernel.h                                                                                                                                                1,1           Tudo
+                                                                                                                                                                                                           
