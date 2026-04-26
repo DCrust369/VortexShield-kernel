865c88cdbf218f26f8dffd5ec49a3ce9767fbf61
DCrust369 <DCrust@proton.me>
2026-04-26 12:05:50 +0100

    drives_library.C

---
+++ drives_PC/drives_library.C
@@ -0,0 +1,7 @@
+#include <CUDA> // this is driver for nvidea
+#include <cuDNN> // other drive for nvidea
+#include <HIP> // drive for amd
+#include <ROCm> // other drive for amd
+#include <oneAPI> // other drive for intel
+#include <OpenCL> // others
+#include <Vulkan> // others
