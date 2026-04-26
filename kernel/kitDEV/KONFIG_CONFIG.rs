ef05da43c7edb30e0e1b710c06e5b29a028cec37
DCrust369 <DCrust@proton.me>
2026-04-26 14:44:06 +0100

    Create KONFIG_CONFIG.rs

---
+++ kernel/kitDEV/KONFIG_CONFIG.rs
@@ -0,0 +1,11 @@
+use::code; // use for coding
+use::github; // save the code
+use::gitlab; // save the code with privacy
+use::git; // save code on terminal
+use::linux; 
+use::compiler; // for code
+use::updates;
+use::upgrades;
+use::no_telemetry; // plus privacy
+use::VPN;
+use::languages; // your language for creat
