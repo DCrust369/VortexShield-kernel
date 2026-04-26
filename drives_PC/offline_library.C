e6d0346c95fcbb55dd1fe2eda77b074bba0162b5
DCrust369 <DCrust@proton.me>
2026-04-26 13:01:27 +0100

    offline_library.C

---
+++ drives_PC/offline_library.C
@@ -0,0 +1,11 @@
+if else () void {
+    struct(asm(intel));
+    struct(asm(amd));
+    struct(asm(nvidea));
+    continue lua: u128 = sudo rct install; // in the final is a packge
+    continue driver_hardware.zig: u64 = true;
+    #include <driver_hardware.zig>
+    continue drives_library.C = true;
+    #include <driver_library.C>
+    continue scam(hardware));
+}
