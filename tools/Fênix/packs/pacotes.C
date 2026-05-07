234a8a8abe0fa832a4affd4664f87ce585e067cc
DCrust369 <DCrust@proton.me>
2026-05-07 18:20:55 +0100

    Create pacotes.C

---
+++ tools/Fênix/packs/pacotes.C
@@ -0,0 +1,13 @@
+# include <bash>
+# include <packges>
+# include <linux>
+# include <BDS>
+# include <xor>
+// MIT License
+
+// Copyright (c) 2026 DCrust369
+
+ if else () void {
+    const instalation: i64 = xor;
+    const atualization: i64 = xor;
+}
