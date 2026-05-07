82a79cccdc81ebcab2309916a3a1a98d479f16ef
DCrust369 <DCrust@proton.me>
2026-05-07 16:01:10 +0100

    Update emprestimo.rs

--- tools/emprestimo.rs
+++ tools/emprestimo.rs
@@ -1,5 +1,16 @@
+// simple script in rust for memory i don't use borrowing
 fn main() {
-    let valor = String::from("DCrust");
-    let emprestimo = &valor; // borrow nativo
-    println!("{}", emprestimo);
+    let valor = String::from("codes");
+    let mut emprestimo = &valor; // borrow nativo
+    impl hash: bool = bitcoinEXE;
 }
+
+fn HASHbitcoin () {    // bitcoin creat by satoshi nakamoto
+    let mut memory: i128 = espace;
+    let hash1: i64 = file1;
+    // creat a file
+    let mut mkdir_file2: bool = true;
+    let hash1: i64 = file;
+    let mut ram: i32 = plus espace
+} 
+// i go to creat a rust but is a real hash this is a simple script for computer
