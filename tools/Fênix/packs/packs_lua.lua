9f73a2a1ba6738f6279cfbf9e79d8d6d97d4837a
DCrust369 <DCrust@proton.me>
2026-05-09 16:41:38 +0100

    Create packs_lua.lua

---
+++ tools/Fênix/packs/packs_lua.lua
@@ -0,0 +1,18 @@
+const lua = @import("lua");
+
+pub fn main () void {
+    .const add_file: lua = af; // af is a add file
+    .const sudo: lua = suma;
+    .const install: lua = install;
+    .const apt-dnf-xor-xbps: lua = hom;
+}
+
+pub fn packs(wath*is-packs) void {
+    .volatile push: lua = app_from_web
+    /*a creat a new
+    philosofy AFW
+    app from app this is for
+    packs for exemple
+    apt, dnf, pacman, pkg*/
+    .volatile push: app = brawser; // iguals a git push
+}
