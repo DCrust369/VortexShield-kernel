a7dc986535b9cd1bae81aef472063e6f85955561
DCrust369 <DCrust@proton.me>
2026-05-09 16:21:23 +0100

    Create KONFIG_CONFIG_gaming.c

---
+++ kernel/kitDEV/kitGAMER/KONFIG_CONFIG_gaming.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-or-later OR Apache-2.0 OR MIT
+// Copyright (c) 2026 DCrust
+// VortexShield-kernel - Focused on Cybersecurity and Pentesting
+
+#include <steam>
+#include <nvidia>
+#include <controler>
+#include <emulator>
+#include <wi-fi_fast>
+#include <no-hack>
