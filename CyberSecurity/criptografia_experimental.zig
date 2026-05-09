39eda89b289d6abea972c9fe875acf58b285d3b9
DCrust369 <DCrust@proton.me>
2026-05-09 16:10:18 +0100

    Update criptografia_experimental.zig

--- CyberSecurity/criptografia_experimental.zig
+++ CyberSecurity/criptografia_experimental.zig
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later OR Apache-2.0 OR MIT
+// Copyright (c) 2026 DCrust
+// VortexShield-kernel - Focused on Cybersecurity and Pentesting
+
 const cryptografy = @import("cryptografy");
 
 pub fn main () void {
