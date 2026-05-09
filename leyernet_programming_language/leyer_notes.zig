1c8bab0c2daa0cb9efaa937eb2dd61383e846560
DCrust369 <DCrust@proton.me>
2026-05-09 16:11:26 +0100

    Update leyer_notes.zig

--- leyernet_programming_language/leyer_notes.zig
+++ leyernet_programming_language/leyer_notes.zig
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later OR Apache-2.0 OR MIT
+// Copyright (c) 2026 DCrust
+// VortexShield-kernel - Focused on Cybersecurity and Pentesting
+
 const std = @import("std"); // i whiting a leyernet language
 
 pub fn main ()void {
