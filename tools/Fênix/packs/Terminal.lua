4b60fba8336b4c1563da440bdbbdb4d8a2f5fce2
DCrust369 <DCrust@proton.me>
2026-05-09 16:34:23 +0100

    Create Terminal.lua

---
+++ tools/Fênix/packs/Terminal.lua
@@ -0,0 +1,11 @@
+sudo xor update -y
+sudo xor install lua5.4 -y
+
+sudo apt update -y
+sudo apt install lua5.4 -y
+
+sudo dnf update -y
+sudo dnf install lua5.4 -y
+
+sudo pkg update -y
+sudo pkg install lua5.4 -y
