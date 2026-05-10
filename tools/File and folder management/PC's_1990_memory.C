a48d5256926d3b19ddb2e8986e8730f6a269c10e
DCrust369 <DCrust@proton.me>
2026-05-10 20:38:35 +0100

    Create PC's_1990_memory.C

---
+++ tools/File and folder management/PC's_1990_memory.C
@@ -0,0 +1,23 @@
+#include <16-colours>
+#include <30MB> // FOR HARD DISK
+#include <128-KB> // for RAM memory
+#include <studio.h>
+/*
+for PC's 1990
+*/
+continue asm () {
+    const 30mb: i32 = (terminal(bash(code))); // 15 sobre 16 mais 2 e era um buffer overflow então mudei para i32
+    const 128kb: i16 = (shell(bash)); // longe de um buffer overflow
+    const 16kb: i16 = (blue(gree())); // 15 para um i16 foi quase!
+    const 8kb: i8 = (red()); // nossa um 8 direitinho!
+}
+/*
+como que as pessoas de
+antigamente faziam
+isso!? me senti o
+MacGyver do codigo apenas
+para gerenciar
+este codigo foi feito
+para o computador mais
+frado de 1990
+*/
