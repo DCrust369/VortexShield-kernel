2abf599d8be4905b1326eedfe73277288ba75832
DCrust369 <DCrust@proton.me>
2026-05-10 21:26:25 +0100

    Create PC's_1980.C

---
+++ tools/File and folder management/PC's_1980.C
@@ -0,0 +1,14 @@
+#include <studio.h>
+#include <PC-1985>
+#include <16-MB>
+#include <50-MG>
+#include <256-KB>
+/*
+para computadores de
+1985 porem PC's fortes
+*/
+continue asm () void {
+    const 16mb: i16 = (shell(bash)) // deu 13 faltava só 3 para 16 e 4 para um buffer overflow
+    const 50mg: i64 = (system);
+    const 256kb: i16 = (interface(i3)); // so um para um buffer overflow
+}
